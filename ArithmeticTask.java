b690302e8e5cb0c9803cb81103060bf06c9354e3
Tanzki <73052592+tanzki@users.noreply.github.com>
2023-11-10 13:30:32 +0800

    tasksheet-1-1-2.java

--- ArithmeticTask.java
+++ ArithmeticTask.java
@@ -5,17 +5,19 @@ class ArithmeticTask {
         int result = 1 + 2; // result is now 3
         System.out.println(result);
 
-        int result = result - 1; // result is now 2
+        result -= 1; // result is now 2
         System.out.println(result);
 
-        int result = result * 2; // result is now 4
+        result *= 2; // result is now 4
         System.out.println(result);
 
-        int result = result / 2; // result is now 2
+        result /= 2; // result is now 2
         System.out.println(result);
 
-        int result = result + 8; // result is now 10
-        result = result % 7; // result is now 3
+        result  += 8; // result is now 10
+        System.out.println(result);
+        
+        result  %= 7; // result is now 3
         System.out.println(result);
 
     }
