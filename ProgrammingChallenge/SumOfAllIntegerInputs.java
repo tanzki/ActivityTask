52d8f5023e84f0c98e43db1fdc63fe9104b123d2
Tanzki <73052592+tanzki@users.noreply.github.com>
2023-11-28 23:52:59 +0800

    Update SumOfAllIntegerInputs.java

--- ProgrammingChallenge/SumOfAllIntegerInputs.java
+++ ProgrammingChallenge/SumOfAllIntegerInputs.java
@@ -3,22 +3,20 @@ package ProgrammingChallenge;
 import java.util.Scanner;
 
 public class SumOfAllIntegerInputs {
-
 	public static void main(String[] args) {
-	Scanner scanner = new Scanner(System.in);
+		Scanner scanner = new Scanner(System.in);
+		
+		int sum = 0;
 
-	int sum = 0;
+    		System.out.println("Enter integers (enter a non-integer value to stop):");
 
-    System.out.println("Enter integers (enter a non-integer value to stop):");
+    		while (scanner.hasNextInt()) {
+        		int num = scanner.nextInt();
+        		sum += num;
+    		}
 
-    while (scanner.hasNextInt()) {
-        int num = scanner.nextInt();
-        sum += num;
-    }
+    		System.out.println("The sum of all the integers entered is: " + sum);
 
-    System.out.println("The sum of all the integers entered is: " + sum);
-
-    scanner.close();
     }
 
 }
