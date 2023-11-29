52479c61ed4bcea21dd8dc8ba55679ba8d9eb38d
Tanzki <73052592+tanzki@users.noreply.github.com>
2023-11-29 13:03:58 +0800

    Add files via upload

---
+++ ProgrammingChallenge/MultiplicationTable10x10.java
@@ -0,0 +1,20 @@
+package ProgrammingChallenge;
+
+public class MultiplicationTable10x10 {
+	public static void main(String[] args) {
+		System.out.println("MULTIPLICATION TABLE");
+
+		System.out.println(); 
+		
+		 for (int a =1; a <= 10; a++) {
+		   
+		        for (int b=1; b <=10; b++) {
+		      
+		            System.out.println(a + " * " + b + " = " + a*b);
+		        }
+		        System.out.println("==============");
+		    }
+	
+	}
+	
+}
