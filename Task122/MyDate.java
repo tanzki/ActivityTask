6607750cdc0a40ea6bc11983572e35d61bc4fe33
Tanzki <73052592+tanzki@users.noreply.github.com>
2023-12-02 13:36:42 +0800

    Add files via upload

---
+++ Task122/MyDate.java
@@ -0,0 +1,15 @@
+package Task122;
+
+public class MyDate {
+
+	public static void main(String[] args) {
+	    DateTask date1 = new DateTask(1,1,1978);
+        DateTask date2 = new DateTask(9,21,1984);
+        System.out.println(date1.toString());
+        System.out.println(date2.toString());
+       // display leap years
+        DateTask.leapYears();
+        
+	}
+
+}
